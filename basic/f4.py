7e9c3da6c9040d8d67c5754d4679f432081579b2
hyelim0323 <halley0323@duksung.ac.kr>
2023-03-23 14:46:56 +0900

    feat: get 방식으로 데이터를 전송하는 포맷

--- basic/f4.py
+++ basic/f4.py
@@ -18,14 +18,29 @@
                 })
         - 서버
             - get 방식 데이터 추출
+            - name = request.args.get('name')
+            - age = request.args.get('age')
+        
+        - /link쪽으로 요청하는 방식은 다양할 수 있다. 단 사이트 설계상 1가지로만 정의되어 있다면
+        다른 방식의 접근은 모두 비정상적인 접근이다(웹 크롤링, 스크래핑, 해킹 등이 대상)
+        이런 접근을 필터링 할 것인가? 보안의 기본사항
 '''
 from flask import Flask, render_template, jsonify, request, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "helloworld"
+@app.route('/link')
+def link():
+    # request.args['age'] => 데이터 누락시 서버 셧다운됨, 사용하면 안됨
+    # request.args.get('age') => 데이터 누락시 None이 나와서 예외처리 가능함
+    name = request.args.get('name')
+    age = request.args.get('age')
+    return "[%s] [%s]" % (name, age)
+
+@app.route('/test')
+def test():
+    # 엔트리포인트(진입로, 프로그램 시작점)과 같은 경로에 templates/test.html 생성
+    return render_template('test.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
