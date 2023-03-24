5b85e764a05d2fe4571e9665008da08d1321e71f
hyelim0323 <halley0323@duksung.ac.kr>
2023-03-24 13:52:43 +0900

    feat: 데이터베이스 연결 및 접속종료 기본형

--- basic/d1.py
+++ basic/d1.py
@@ -81,5 +81,63 @@
         ENGINE=InnoDB
         ;
 
+        6. 회원가입 -> insert
+            - insert into <데이터베이스명>, <테이블명> 
+                [(컬럼명, 컬럼명, 컬렴명, ...)] 
+              values 
+                (값, 값, 값, ...);
+            - '키워드를 테이블명, 컬럼명 등등에 사용하면 백틱을 통해서 처리 가능'
+            - '값'
+            - now() : mysql의 내장함수로써 현재시간을 리턴
+            - 
+                INSERT INTO 
+                    `ml_db`.`users` 
+                    (`uid`, `upw`, `name`, `regdate`) 
+                VALUES 
+                    ('guest', '1234', '게스트', now());
+
+        7. 회원 정보 수정 -> update
+
+        8. 회원 탈퇴 -> delete
+            - 1년간 보관?, 완전 삭제?
+        
+        9. 로그인 -> select (쿼리의 분량이 가장 많다)
+            -- 회원 가입 여부 조회 -> 로그인 처리 쿼리
+            -- 대소문자 구분 안한다
+            -- 제시한 아이디와 비번과 일치하는 row 데이터를
+            -- 가져와서 uid, name, regdate 값을 출력하시오
+            SELECT 
+                uid, `name`, regdate
+            FROM
+                users
+            WHERE
+                uid='guest' 
+            AND
+                upw = '1234
+        
+'''
+'''
+    파이썬에서 DB에 접속, 접속해제
 '''
 import pymysql as my
+
+connection = None
+try: 
+    # 1. 접속
+    connection = my.connect(host='localhost',   # 127.0.0.1, 서버주소
+                            #port = 3306, # 포트 (기본값)
+                            user='root',    # 사용자 계정, root 계정 외의 계정 사용 권장
+                            password='2658374',  # 비밀번호
+                            database='ml_db',   # 접속할 데이터베이스
+                            #cursorclass=my.cursors.DictCursor
+                            )
+    print('접속 성공')
+except Exception as e:
+    print('접속 오류', e)
+else:
+    print('접속시 문제 없었음')
+finally:
+    # 2. 접속 종료(I/O) -> close()
+    if connection:
+        connection.close()
+    print('접속 종료 성공')
